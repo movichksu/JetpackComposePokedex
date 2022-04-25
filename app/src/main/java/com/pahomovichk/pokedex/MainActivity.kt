41915dc6a4841a51fc91097cd0c8dce08bd44370
Kseniya Pakhamovich <kseniya.pakhamovich@innowise-group.com>
2022-04-25 17:17:35 +0300

    Initial commit

---
+++ app/src/main/java/com/pahomovichk/pokedex/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.pahomovichk.pokedex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import com.pahomovichk.pokedex.ui.theme.PokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PokedexTheme {
+
+            }
+        }
+    }
+}
